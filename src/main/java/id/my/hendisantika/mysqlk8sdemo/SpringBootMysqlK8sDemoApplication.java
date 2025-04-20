b9469a08a85c71b7d483d6d3d38b3c482732f317
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 22:30:25 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/mysqlk8sdemo/SpringBootMysqlK8sDemoApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.mysqlk8sdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlK8sDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMysqlK8sDemoApplication.class, args);
+    }
+
+}

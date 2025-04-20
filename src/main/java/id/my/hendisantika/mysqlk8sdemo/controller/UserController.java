11d1964e2c2487707f3d1593ec42a1f43026a306
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 22:35:00 +0700

    feat: Add getUsers in UserController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/mysqlk8sdemo/controller/UserController.java
+++ src/main/java/id/my/hendisantika/mysqlk8sdemo/controller/UserController.java
@@ -3,9 +3,12 @@ package id.my.hendisantika.mysqlk8sdemo.controller;
 import id.my.hendisantika.mysqlk8sdemo.model.User;
 import id.my.hendisantika.mysqlk8sdemo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,5 +31,10 @@ public class UserController {
     public String saveUser(@RequestBody User emp) {
         userRepository.save(emp);
         return "User added successfully::" + emp.getId();
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 }

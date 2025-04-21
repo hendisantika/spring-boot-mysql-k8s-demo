c9de7a095c8a47a8253a7fabe6446cbe08e78622
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 17:39:58 +0700

    feat: Add shouldSaveUser in UserRepositoryIntegrationTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/mysqlk8sdemo/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/id/my/hendisantika/mysqlk8sdemo/repository/UserRepositoryIntegrationTest.java
@@ -1,10 +1,14 @@
 package id.my.hendisantika.mysqlk8sdemo.repository;
 
 import id.my.hendisantika.mysqlk8sdemo.config.AbstractContainerBaseTest;
+import id.my.hendisantika.mysqlk8sdemo.model.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 @Testcontainers
@@ -14,5 +18,20 @@ class UserRepositoryIntegrationTest extends AbstractContainerBaseTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Test
+    void shouldSaveUser() {
+        // Given
+        User user = new User();
+        user.setName("Itadori Yuji");
+        user.setCountry("JPN");
 
+        // When
+        User savedUser = userRepository.save(user);
+
+        // Then
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getId()).isNotNull();
+        assertThat(savedUser.getName()).isEqualTo("Itadori Yuji");
+        assertThat(savedUser.getCountry()).isEqualTo("JPN");
+    }
 }

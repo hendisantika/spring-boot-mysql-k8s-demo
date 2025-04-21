5b41f5538f1e545d033cf2e0a3fef712bfdc69c4
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 17:32:36 +0700

    feat: Add TestContainers configs🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/mysqlk8sdemo/SpringBootMysqlK8sDemoApplicationTests.java
+++ src/test/java/id/my/hendisantika/mysqlk8sdemo/SpringBootMysqlK8sDemoApplicationTests.java
@@ -1,10 +1,13 @@
 package id.my.hendisantika.mysqlk8sdemo;
 
+import id.my.hendisantika.mysqlk8sdemo.config.AbstractContainerBaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-class SpringBootMysqlK8sDemoApplicationTests {
+@Testcontainers
+class SpringBootMysqlK8sDemoApplicationTests extends AbstractContainerBaseTest {
 
     @Test
     void contextLoads() {

9da9cef9609d09540e98e6ee4d625251dd33bec8
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 17:32:48 +0700

    feat: Add AbstractContainerBaseTest🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/mysqlk8sdemo/config/AbstractContainerBaseTest.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.mysqlk8sdemo.config;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+public abstract class AbstractContainerBaseTest {
+
+    private static final String MYSQL_IMAGE = "mysql:9.3.0";
+    private static final String DATABASE_NAME = "test";
+    private static final String USERNAME = "testuser";
+    private static final String PASSWORD = "testuser@123";
+
+    @Container
+    protected static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>(MYSQL_IMAGE)
+            .withDatabaseName(DATABASE_NAME)
+            .withUsername(USERNAME)
+            .withPassword(PASSWORD)
+            .withReuse(true);
+
+    static {
+        mySQLContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", mySQLContainer::getDriverClassName);
+    }
+}

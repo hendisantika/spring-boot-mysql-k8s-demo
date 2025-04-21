21fb5ba3510b944ce6a684fe79bd85718bef43ed
hendisantika <hendisantika@yahoo.co.id>
2025-04-21 17:34:55 +0700

    feat: Add shouldCreateUser in UserControllerIntegrationTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/mysqlk8sdemo/controller/UserControllerIntegrationTest.java
+++ src/test/java/id/my/hendisantika/mysqlk8sdemo/controller/UserControllerIntegrationTest.java
@@ -2,13 +2,22 @@ package id.my.hendisantika.mysqlk8sdemo.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import id.my.hendisantika.mysqlk8sdemo.config.AbstractContainerBaseTest;
+import id.my.hendisantika.mysqlk8sdemo.model.User;
 import id.my.hendisantika.mysqlk8sdemo.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,5 +38,22 @@ class UserControllerIntegrationTest extends AbstractContainerBaseTest {
         userRepository.deleteAll();
     }
 
+    @Test
+    void shouldCreateUser() throws Exception {
+        // Given
+        User user = new User();
+        user.setName("Itadori Yuji");
+        user.setCountry("JPN");
 
+        // When
+        ResultActions response = mockMvc.perform(post("/addUser")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)));
+
+        // Then
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isString())
+                .andExpect(jsonPath("$").isNotEmpty());
+    }
 }
